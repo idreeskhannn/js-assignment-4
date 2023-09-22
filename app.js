422b6c2e02b2c796d9918ae762e9bf46c917d2f1
idrees khan <midreeskhan7276@gmail.com>
2023-09-22 22:17:31 +0500

    done

---
+++ app.js
@@ -0,0 +1,268 @@
+// alert("hello world")
+
+    //?  chapter 17 to  20   /////////
+ 
+// Question  NO 1//
+
+// var arr = [
+//     [],
+//     []
+// ]
+
+// Question  NO 2 //
+
+
+// var arr = [
+// //     [0,1,2,3],
+// //     [1,0,1,2],
+// //     [2,1,0,1]
+
+// // ]
+// // for (var i = 0 ; i < 3 ; i++){
+// //     document.write(arr[i].join(" ") + "<br>")
+// // }
+
+// Question  NO 3 //
+
+// for (var i = 1 ; i <=10 ; i++)
+// {
+//     document.write(i +"<br>")
+// }
+
+// Question  NO 4//
+
+// var user =+prompt("enter ur table numbr")
+// var length =+prompt("enter ur length numbr")
+// for (var i= 1 ; i<=length ; i++ ){
+//     document.write(user + " x " + i + " = " + user * i + "<br>")
+// }
+
+// Question  NO 5 //
+
+// var arr =["apple" , "banana" ," mango " ," orange"  ]
+// for (var i= 0 ; i<arr.length ; i++ )
+// {
+//     document.write("Elemrnt at index "+i+ " is "  +arr[i] + "<br>")
+// }
+
+// Question  NO 6 //
+
+// for (var i = 1 ; i <=15 ; i++)
+// {
+//     document.write(i )
+// }
+
+// for (var i = 15 ; i >=1 ; i--)
+// {
+//     document.write(i +" ")
+// }
+
+
+
+// for (var i = 2 ; i <=20 ; i =i+2)
+// {
+//     document.write(i +" ")
+// }
+
+// for(var i = 1 ; i <=20 ; i =i+2)
+// {
+//     document.write(i +" ")
+// }
+
+// for (var i = 2 ; i <=20 ; i =i+2)
+// {
+//     document.write(i +"k ")
+// }
+
+// Question  NO 7 //
+
+// var user = prompt("enter item")
+// var isMatch= "no"
+// var items = ["cake" ,"coca cola " , "juice" , "biscuit"]
+//  for (var i = 0 ; i<items.length ; i++){
+//      if(user === items[i]){
+//          alert(user + " is available at index " + i +" in our bakery ")
+//     isMatch= "yes"
+//         }
+//  }if (isMatch == "no"){
+//      alert("not available")
+//  }
+
+// Question  NO  8 //
+
+
+// var arr =[10,100,300,60]
+// document.write(arr +"<br>")
+
+// var arr =[10,100,300,60]
+// var min = Math.min.apply(null, arr),
+//     max = Math.max.apply(null, arr);
+// document.write(max)
+
+
+// Question  NO 9//
+
+
+// var arr =[10,100,300,60]
+// document.write(arr +"<br>")
+
+// var arr =[10,100,300,60]
+// var min = Math.min.apply(null, arr)
+// document.write(min)
+
+
+// Question  NO 10 //
+
+// for (var i = 5 ; i <=100 ; i =i+5){
+        
+    
+// document.write(i +"<br>")
+// }
+
+
+      //?     CHAPTER 21 TO 25  /////
+
+ // STRING METHODS
+ 
+ // question = 1 //
+ 
+ 
+ // var firstName = prompt("Enter Your FirstName");
+ // var lastName = prompt("Enter Your LastName");
+ // document.write(firstName + " " + lastName);
+ 
+ // question = 2 //
+ 
+ 
+ // var value = prompt("Enter any name");
+ // var value1 = names.length;
+ // document.write(value + " = "+" "+ value1);
+ 
+ // question = 3 //
+
+ // var value = "Pakistani";
+ // var index = value.indexOf("n");
+ // document.write("Index of 'n'" + " "+ index);
+ 
+ 
+ // question = 4 //
+ 
+ // var value = "Hell World";
+ // var value1 = value.lastIndexOf("l");
+ // document.write(" Last index of 'l'" + " "+ value1);
+ 
+ // qustion = 5 //
+ 
+ // var value = "Pakistani";
+ // var chart = value.charAt(3);
+ // document.write("Character at  index '3'" + " "+ chart);
+ 
+ // question = 6 //
+ 
+ // var firstName = prompt("Enter Your FirstName");
+ // var lastName = prompt("Enter Your LastName");
+ // var conc = firstName.concat(lastName);
+ // document.write(conc);
+ 
+ // question = 7 //
+ 
+ // var value = "Hyderbad";
+ // var rep = value.replace("Hyder","Islam");
+ // document.write(rep);
+ 
+ //  questtion  = 8 //
+ 
+ // var value = "Ali and Sami are best friends. They play cricket and football together.";
+ // var rep = value.replace(/and/g ,"&");
+ // document.write(rep);
+  
+ 
+ // question  = 9 //
+ 
+ // var value = "472";
+ // var hu = Number(value);
+ // document.write(hu);
+ 
+ 
+ // question = 10 //
+ 
+ // var value = prompt("Enter Your Name").toUpperCase();
+ // document.write(value)
+ 
+ // question = 11 //
+ 
+ // var user =prompt("Enter Your Name");
+ 
+ // var name = user[0].toUpperCase();
+ // var othername = user.slice(1).toLowerCase();
+ // document.write(name+othername);
+ 
+ 
+ // question  = 12 //
+ 
+ 
+ // var num = 35.36;
+ // var strin =num.toString().replace(".", ""); 
+ // document.write(strin)
+ 
+ 
+ // question = 13 //
+ 
+//  var user = prompt("Enter special name").trim;
+//  var arr =["@","!",",",".","$"];
+//  for(var a =0; a<arr.length ; a++ ){
+//      if(user === arr[a]){
+//          alert("invalid")
+
+//      }
+//  }
+//  var user = prompt("enter ur name").trim();
+//  for (var i = 0 ; i <user.length ; i++){
+//  if(user.charAt(i) === "@"){
+//      alert("invalid")
+//  }
+//  else if(user.charAt(i) === "$"){
+//      alert("invalid")
+//  }
+//  else if(user.charAt(i) === "."){
+//     alert("invalid")
+// }else if(user.charAt(i) === "#"){
+//     alert("invalid")
+// }
+
+//  }
+ // QUESTION = 14 //
+ 
+ // var user = prompt("enter item").toLowerCase()
+ // // var isMatch= "no"
+ // var items = ["cake " ,"coca cola " , "juice" , "biscuit"]
+ //  for (var i = 0 ; i<items.length ; i++){
+ //      if(user === items[i]){
+ //          alert(user + " is available at index " + i +" in our bakery ")
+ //     isMatch= "yes"
+ //         }
+ //  }if (isMatch == "no"){
+ //      alert("not available")
+ //  }
+ // 
+ 
+ // QUESTION = 15 //
+ 
+ 
+ // QUESTION = 16 //
+ 
+ 
+ 
+ 
+ 
+ // QUESTION = 17 //
+ 
+ 
+ // var names = "Pakistan";
+ // var name1=names.slice(-1);
+ // document.write(name1);
+ 
+ 
+ // QUESTION = 18 //
+ 
+ 
